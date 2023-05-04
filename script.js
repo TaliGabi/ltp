932d21937441bbfc6ea064d4019597291cce0e41
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-05-03 23:59:33 -0400

    att mudanças

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// divListaProduto.insertAdjacentHTML('afterbegin', '');
+divListaProdutoDestaque.insertAdjacentHTML('afterbegin', 'mostrar_produtos_destaque');
+var divListaProdutoDestaque = document.getElementById('produto-destaque');
+divListaProduto.insertAdjacentHTML('afterbegin', 'mostrar_produtos');
+var divListaProduto = document.getElementById('lista-produtos');
 
 class Produto{
   constructor(nome,dataCadastro,descricao,preco){
@@ -9,17 +12,26 @@ class Produto{
   }
 
   mostrar_produtos(){
-    return this.nome + this.dataCadastro + this.descricao + this.preco
+    return `<div>${this.nome}</div>
+    <div>${this.dataCadastro}</div>
+    <div>${this.descricao}</div>
+    <div>${this.preco}</div>`
 }
 }
-
+//
 class ProdutoDestaque extends Produto{
     constructor(){
         super(nome,dataCadastro,descricao,preco,imgDestaque)
     }
     mostrar_produtos_destaque(){
-    return this.nome + this.dataCadastro + this.descricao + this.preco + this.imgDestaque
+    return `<div>${this.nome}</div>
+    <div>${this.dataCadastro}</div>
+    <div>${this.descricao}</div>
+    <div>${this.preco}</div>
+    <div>${this.imgDestaque}</div>`
 }
 }
+
+const produto = new ProdutoDestaque ("Album Face", "03-05","Album Kpop","150,00","www.imagenspng.com.br/wp-content/uploads/2022/04/flork-png-08-600x568.png")
 
 //git init//
