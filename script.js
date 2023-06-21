83ffa008a83bbd6ae687d9c70fa109f0dde86f37
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-06-21 10:38:10 -0400

    commit

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-class Produtos{
+class Produto{
   constructor(nome,dataCadastro,descricao,preco){
-    this.nome = nome
-    this.dataCadastro = dataCadastro
-    this.descricao = descricao
-    this.preco = preco 
-  }
+    nome = this.nome
+    dataCadastro = this.dataCadastro
+    descricao = this.descricao
+    preco = this.preco
+}
 
   mostrar_produtos(){
-    return `<div>${this.nome}</div>
-    <div>${this.dataCadastro}</div>
-    <div>${this.descricao}</div>
-    <div>${this.preco}</div>`
+    try {
+      return this.exibir_produtos();  
+    } catch (error) {
+      console.log(error) 
+  }
 }
+
+exibir_produtos(){
+  if((this.nome != "") && (this.descricao != "") && (this.preco != "") && (this.dataDCadrastro != "")){
+    return `<div>
+    <h1>${this.nome}</h1>
+    <p>${this.dataCadastro}</p>
+    <h2>${this.descricao}</h2>
+    <h2>${this.preco}</h2></div>`
+  }else{
+      throw new MeuError("Campo não preenchido.")
+  }
+ }
 }
-//
-class ProdutoDestaque extends Produtos{
+
+class ProdutoDestaque extends Produto{
     constructor(){
         super(nome,dataCadastro,descricao,preco,imgDestaque)
     }
@@ -24,15 +37,15 @@ class ProdutoDestaque extends Produtos{
         <div>${this.descricao}</div>
         <div>${this.preco}</div>
         <div>${this.imgDestaque}</div>`
-    }
-}
+  }
+}   
 
 //const produto = new ProdutoDestaque ("Album Face", "03-05","Album Kpop","150,00","www.imagenspng.com.br/wp-content/uploads/2022/04/flork-png-08-600x568.png")
-const produtoDois = new Produtos("Album Face", "03-05","Album Kpop","150,00")
+const produtoDois = new Produto("Album Face", "03-05","Album Kpop","150,00");
 //git init//
 
 //let produtosDest = document.getElementById("produto-destaque")
-let produto = document.getElementById("lista-produtos")
+let produtos = document.getElementById("lista-produtos");
 
 //produtosDest.insertAdjacentHTML('afterbegin', 'mostrar_produtos_destaque');
-produto.insertAdjacentHTML('afterbegin', produtoDois.mostrar_produtos());
+produtos.insertAdjacentHTML('afterbegin', produtoDois.exibir_produtos());
